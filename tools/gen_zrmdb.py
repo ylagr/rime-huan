89ec8fab8e1039eb939666f7ccb477466c69be00
ksqsf <i@ksqsf.moe>
2025-04-18 19:18:03 +0200

    dict: 修改梁粱

--- tools/gen_zrmdb.py
+++ tools/gen_zrmdb.py
@@ -1,14 +1,16 @@
 import re
-from collections import defaultdict
+from collections import OrderedDict
 
 DATA_LINE_RE = re.compile(r'^(.)\t[a-z]+;([a-z][a-z])')
-table = defaultdict(list)
+table = OrderedDict()
 
 with open('../moran.chars.dict.yaml', 'r') as f:
     for l in f:
         if m := re.match(DATA_LINE_RE, l):
             char = m[1]
             code = m[2]
+            if char not in table:
+                table[char] = []
             if code not in table[char]:
                 table[char].append(code)
 
